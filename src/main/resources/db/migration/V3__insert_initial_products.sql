2c6852a988b23607716f917e96e75a517845c053
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-05 22:15:19 +0200

    Version 3 insertion in table products

---
+++ src/main/resources/db/migration/V3__insert_initial_products.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (name, price, description)
+VALUES
+    ('Laptop', 1299.99, 'Ordinateur portable professionnel'),
+    ('Smartphone', 799.99, 'Smartphone dernière génération'),
+    ('Keyboard', 99.99, 'Clavier mécanique');
