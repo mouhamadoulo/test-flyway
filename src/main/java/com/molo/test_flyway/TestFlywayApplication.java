da31a0ee821cb2f2c719f6c75ec7f68c69e31a33
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-05 21:01:45 +0200

    Initial commit product api

---
+++ src/main/java/com/molo/test_flyway/TestFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.molo.test_flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molo/test_flyway/TestFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molo.test_flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
